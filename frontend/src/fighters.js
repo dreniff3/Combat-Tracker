021b2cf7b949a0bad21fbb7f8c8dcd7dc93f9798
Don Reniff <donaldreniff@gmail.com>
2024-05-14 15:17:52 -0700

    Add fighter data

---
+++ frontend/src/fighters.js
@@ -0,0 +1,34 @@
+const fighters = [
+    {
+        _id: '1',
+        name: 'Goblin',
+        image: '',
+        initiative: 12,
+        currHealth: 5,
+        maxHealth: 9,
+        armor: 11,
+        conditions: [],
+    },
+    {
+        _id: '2',
+        name: 'Wolf',
+        image: '',
+        initiative: 17,
+        currHealth: 5,
+        maxHealth: 5,
+        armor: 8,
+        conditions: [],
+    },
+    {
+        _id: '3',
+        name: 'Slime',
+        image: '',
+        initiative: 2,
+        currHealth: 11,
+        maxHealth: 11,
+        armor: 11,
+        conditions: [],
+    },
+];
+
+export default fighters;
