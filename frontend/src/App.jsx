5fae8b7c339794e9f96a0988e5a682156354be5c
Don Reniff <donaldreniff@gmail.com>
2024-05-14 13:50:07 -0700

    Import Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 const App = () => {
-  return (
-    <h1>Combat Tracker</h1>
-  )
+    return (
+      <Header turn={0} />
+    );
 };
 
 export default App;
