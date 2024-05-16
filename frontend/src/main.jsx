339f0b8a11162191e7b3c3cce0a04a162ebadbf4
Don Reniff <donaldreniff@gmail.com>
2024-05-16 15:56:12 -0700

    Add router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App.jsx'
+import CombatScreen from './screens/CombatScreen';
 import './index.css'
+import fighters from './fighters';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<CombatScreen fighters={fighters} />} />
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
