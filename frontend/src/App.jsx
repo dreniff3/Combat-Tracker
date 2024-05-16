9648c0b9d9dfa3921d017fbfd1a950b558e11f53
Don Reniff <donaldreniff@gmail.com>
2024-05-16 14:12:45 -0700

    Change name of Header prop and of component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './components/Header';
-import AddFightersScreen from './screens/AddFightersScreen';
+import FighterList from './components/FighterList';
+import fighters from './fighters';
 
 const App = () => {
     return (
       <>
-        <Header turn={0} />
-        <AddFightersScreen />
+        <Header round={0} />
+        <FighterList fighters={fighters} turn={2} />
+        {/* Add Fighter Btn */}
+        {/* Next Turn Btn */}
+        {/* End Combat Btn */}
       </>
     );
 };
