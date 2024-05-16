d67c84ec2548c0baee5e3987fe9a9a631a0627a4
Don Reniff <donaldreniff@gmail.com>
2024-05-16 15:02:06 -0700

    Change component to Combat Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import Header from './components/Header';
-import FighterList from './components/FighterList';
+import Combat from './screens/Combat';
 import fighters from './fighters';
 
 const App = () => {
-    return (
-      <>
-        <Header round={0} />
-        <FighterList fighters={fighters} turn={2} />
-        {/* Add Fighter Btn */}
-        {/* Next Turn Btn */}
-        {/* End Combat Btn */}
-      </>
-    );
+  return (
+    <>
+      <Combat fighters={fighters} />
+      {/* Add Fighter Btn */}
+      {/* End Combat Btn */}
+    </>
+  );
 };
 
 export default App;
