74ec76f9e668971d5aa1f248f08602f118bc0408
Don Reniff <donaldreniff@gmail.com>
2024-05-20 15:00:19 -0700

    Add AddFighters component

---
+++ frontend/src/components/AddFighters.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const AddFighters = ({ fighters, setFighters }) => {
+    const [name, setName] = useState('');
+    const [initiative, setInitiative] = useState(0);
+    const [armor, setArmor] = useState(0);
+    const [maxHealth, setMaxHealth] = useState(0);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // don't add fighters without required inputs
+        if (name === '' || initiative === 0 || armor === 0 || maxHealth === 0) {
+          return false;
+        }
+
+        let fighter = {
+            name, 
+            initiative,
+            armor,
+            currHealth: maxHealth,
+            maxHealth,
+        };
+        setFighters(fighters => [...fighters, fighter].sort((a, b) => b.initiative - a.initiative));
+        setName('');
+        setInitiative(0);
+        setArmor(0);
+        setMaxHealth(0);
+    };
+    
+    return (
+<div className="add-form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-inputs">
+            <div id="name">
+              <label htmlFor="name">Name: </label>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div id="initiative">
+              <label htmlFor="init">Initiative: </label>
+              <input 
+                type="number"
+                name="init"
+                value={initiative}
+                onChange={(e) => setInitiative(e.target.value)}
+              />
+            </div>
+            <div id="armor">
+              <label htmlFor="armor">Armor Class: </label>
+              <input 
+                type="number"
+                name="armor"
+                value={armor}
+                onChange={(e) => setArmor(e.target.value)}
+              />
+            </div>
+            <div id="health">
+              <label htmlFor="health">Health: </label>
+              <input 
+                type="number"
+                name="health"
+                value={maxHealth}
+                onChange={(e) => setMaxHealth(e.target.value)}
+              />
+            </div>
+          </div>
+          <input type="submit" value="Add Player" />
+        </form>
+      </div>
+    );
+};
+
+export default AddFighters;
