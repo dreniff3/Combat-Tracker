ef145318c7882cfd53943e8c1eca4b6a1006d0e2
Don Reniff <donaldreniff@gmail.com>
2024-05-20 15:01:31 -0700

    Fix functionality for ending combat and disable buttons

--- frontend/src/screens/CombatScreen.jsx
+++ frontend/src/screens/CombatScreen.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import FighterList from '../components/FighterList';
+import AddFighters from '../components/AddFighters';
 import { PiSword } from "react-icons/pi";
 import { FaSkullCrossbones } from "react-icons/fa";
 
-const CombatScreen = ({ fighters }) => {
+const CombatScreen = () => {
+    const [fighters, setFighters] = useState([]);
     const [turn, setTurn] = useState(0);
     const [round, setRound] = useState(0);
     let header;
@@ -14,8 +16,13 @@ const CombatScreen = ({ fighters }) => {
         header = 'Round ' + String(round);
     }
 
+    const endCombat = () => {
+        setRound(0);
+        setFighters([]);
+    }
+
     useEffect(() => {
-        if (turn === fighters.length) {
+        if (fighters.length > 1 && turn === fighters.length) {
             setTurn(0);
             setRound(round + 1);
         }
@@ -32,20 +39,32 @@ const CombatScreen = ({ fighters }) => {
             <header>
                 <h1>{header}</h1>
             </header>
-            <FighterList fighters={fighters} turn={turn} />
+            {round === 0? (
+                <AddFighters 
+                    fighters={fighters}
+                    setFighters={setFighters}
+                />
+            ) : (
+            <FighterList 
+                fighters={fighters}
+                turn={turn}
+            />
+            )}
             {/* Next Turn Btn */}
             <div className="buttons">
                 <button
+                    style={round === 0 ? ({display: 'none'}) : ({display: 'inline-block'})}
                     disabled={round === 0 ? true : false}
                     onClick={() => setTurn(turn + 1)}
                 >Next Turn</button>
                 {round === 0 ? (
                 <button
+                    disabled={fighters.length < 2 ? true : false}
                     onClick={() => setRound(1)}
                 >Start Combat <PiSword /></button>
                 ) : (
                 <button
-                    onClick={() => setRound(0)}
+                    onClick={endCombat}
                 >End Combat <FaSkullCrossbones /></button>
                 )}
             </div>
