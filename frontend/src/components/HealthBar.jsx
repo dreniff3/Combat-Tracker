6230bef8fbab6ee87dd936c542e5372a33f155ec
Don Reniff <donaldreniff@gmail.com>
2024-05-21 09:49:15 -0700

    Add HealthBar component

---
+++ frontend/src/components/HealthBar.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+const HealthBar = ({ currHealth, maxHealth }) => {
+    let healthPercentage = (Math.floor(100 * currHealth / maxHealth));
+    let healthColor;
+    healthPercentage < 30 ? healthColor = '#DE2802' 
+    : healthPercentage < 100 ? healthColor = '#F0DA0E' 
+    : healthColor = '#21CA20';
+    let healthStyles = {
+        width: `${healthPercentage}%`,
+        height: '100%',
+        background: `${healthColor}`,
+        borderRadius: '15px',
+    };
+
+    useEffect(() => {
+        healthPercentage = (Math.floor(100 * currHealth / maxHealth));
+    }, [currHealth])
+
+    return (
+        <div className="health-container">
+            <div className="health-container--bar" style={healthStyles}></div>
+        </div>
+    );
+};
+
+export default HealthBar;
